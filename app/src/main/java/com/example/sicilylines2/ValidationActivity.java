87f3978d977e795cbf9857a77fe810fc27f477b7
abderrahimx05 <75130312+abderrahimx05@users.noreply.github.com>
2023-04-10 03:05:58 +0200

    create first 5 pages of this Android's app

---
+++ app/src/main/java/com/example/sicilylines2/ValidationActivity.java
@@ -0,0 +1,13 @@
+package com.example.sicilylines2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ValidationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_validation);
+    }
+}
